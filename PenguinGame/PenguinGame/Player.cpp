9a187ade3cdeed45f32bfe079afebf4113bfc259
Ifrim Diana <diana.ifrim@student.unitbv.ro>
2024-10-31 18:44:44 +0200

    Update Player and Penguin Implementation

--- PenguinGame/PenguinGame/Player.cpp
+++ PenguinGame/PenguinGame/Player.cpp
@@ -1,7 +1,7 @@
 #include "Player.h"
 
-Player::Player(const std::string& name, Position initial_position)
-	:m_name(name),m_position(initial_position),m_points(0),m_score(0),m_lives(3)
+Player::Player(const std::string& name)
+	:m_name(name),m_points(0),m_score(0)
 
 {
 }
--- PenguinGame/PenguinGame/Player.h
+++ PenguinGame/PenguinGame/Player.h
@@ -1,23 +1,17 @@
 #pragma once
 #include <iostream>
 
-struct Position {
-	int x, y;
-};
-
 class Player
 {
 private:
 	std::string m_name;
 	int m_points;
 	int m_score;
-	int m_lives;
-	Position m_position;
 public:
-	Player(const std::string& name, Position initial_position);
+	Player(const std::string& name);
 	std::string getName();
 	int getPoints();
-	int getLives();
+	int getScore();
 	~Player();
 };
 
