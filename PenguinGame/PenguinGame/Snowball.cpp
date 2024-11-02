8e0ded5c02d377d7beb1863a091986b817b0e1c2
adinaion <79267944+adinaion@users.noreply.github.com>
2024-11-02 12:57:16 +0000

    Snowball Constructor Implementation

--- PenguinGame/PenguinGame/Snowball.cpp
+++ PenguinGame/PenguinGame/Snowball.cpp
@@ -1,1 +1,6 @@
 #include "Snowball.h"
+
+Snowball::Snowball(std::tuple<int, int> start_position, std::string launch_direction, float launch_speed)
+	: m_position(start_position), m_direction(launch_direction), m_speed(launch_speed), m_active(true)
+{
+}
