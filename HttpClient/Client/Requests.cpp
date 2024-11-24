dd9be8b975e662ef9a93597bd9a880b14a146166
adinaion <79267944+adinaion@users.noreply.github.com>
2024-11-24 14:22:04 +0200

    Updated Requests Class in Client

--- HttpClient/Client/Requests.cpp
+++ HttpClient/Client/Requests.cpp
@@ -1,1 +1,11 @@
 #include "Requests.h"
+
+namespace ClientRequests {
+    cpr::Response CreatePlayer(const std::string& name, const std::string& password) {
+        return cpr::Get(cpr::Url{ "http://localhost:18080/addPlayer/" + name + "/" + password });
+    }
+
+    cpr::Response GetPlayer(const std::string& name) {
+        return cpr::Get(cpr::Url{ "http://localhost:18080/getPlayer/" + name });
+    }
+}
--- HttpClient/Client/Requests.h
+++ HttpClient/Client/Requests.h
@@ -1,5 +1,8 @@
 #pragma once
-class Requests
-{
-};
+#include <string>
+#include <cpr/cpr.h>
 
+namespace ClientRequests {
+    cpr::Response CreatePlayer(const std::string& name, const std::string& password);
+    cpr::Response GetPlayer(const std::string& name);
+}
