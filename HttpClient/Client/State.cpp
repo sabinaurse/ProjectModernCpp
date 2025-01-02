e543a817ed96cf240e0acef3a485bdc9a828a466
adinaion <79267944+adinaion@users.noreply.github.com>
2025-01-02 21:47:52 +0200

    Deleted console client

--- HttpClient/Client/State.cpp
+++
@@ -1,9 +0,0 @@
-#include "State.h"
-
-void ClientState::SetCurrentPlayer(const std::string& name) {
-    m_currentPlayer = name;
-}
-
-std::string ClientState::GetCurrentPlayer() const {
-    return m_currentPlayer;
-}
